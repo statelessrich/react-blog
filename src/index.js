fb163f8c0795f0f7bc2766adc4d71788a49c3ab3
richard pires <richardjpires@gmail.com>
2017-08-04 20:02:28 -0500

    Updated colors and removed unused code.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
-// import * as postActions from './actions/posts-actions';
 import configureStore from './store/configureStore';
+import './index.css';
 
 const store = configureStore();
-//store.dispatch(postActions.getPosts());
 
 render(
   <Provider store={store}>
